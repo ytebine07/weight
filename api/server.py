ea04cc5f6a244669735b08442d9ed65016f9003d
ytebine07 <ytebine07@gmail.com>
2020-04-27 18:31:05 +0900

    add Dcokerfile

--- api/server.py
+++ api/server.py
@@ -6,6 +6,8 @@ class AppResource(object):
 
     def on_get(self, req, resp):
 
+        print("hi")
+
         msg = {
             "message": "hello world"
         }
@@ -17,5 +19,5 @@ app.add_route("/", AppResource())
 
 if __name__ == "__main__":
     from wsgiref import simple_server
-    httpd = simple_server.make_server("127.0.0.1", 8000, app)
+    httpd = simple_server.make_server("0.0.0.0", 8080, app)
     httpd.serve_forever()
