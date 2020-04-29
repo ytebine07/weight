36c8b46c6fa4d7853fcc53fcb908e61dde156f84
ytebine07 <ytebine07@gmail.com>
2020-04-29 14:03:22 +0900

    format

--- api/server.py
+++ api/server.py
@@ -3,14 +3,11 @@ import falcon
 
 
 class AppResource(object):
-
     def on_get(self, req, resp):
 
         print("hi")
 
-        msg = {
-            "message": "hello world"
-        }
+        msg = {"message": "hello world"}
         resp.body = json.dumps(msg)
 
 
@@ -19,5 +16,6 @@ app.add_route("/", AppResource())
 
 if __name__ == "__main__":
     from wsgiref import simple_server
+
     httpd = simple_server.make_server("0.0.0.0", 8080, app)
     httpd.serve_forever()
